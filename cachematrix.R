b15651b3918c9cf7b2fe8c74f23ede19e46dda99
bluesungod <aneil.basu@gmail.com>
2014-04-26 00:50:41 -0400

    Submission R Programming Assignment 2
    Submitting cachematrix.R for peer assessment - week 3.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2: Caching Inverse of a Matrix
 
-## Write a short comment describing this function
+## These functions will try to speed up the process of
+# getting the inverse of a matrix.
+
+## First we will create a special object that we can use
+# to cache an inverse. Then in another function, we will
+# compute the inverse of the special object "matrix" after
+# first checking to see if it is already in the cache.
+
+
+## To start, let's create the special object, which will
+# help us by caching the inverse of itself.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    ## The <<- operator assigns a value outside the given
+    # environment.
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  ## This code returns the "special object" to the console,
+  # which is a list that contains functions. We can use the
+  # functions contained as tools to help us calculate the
+  # inverse of the matrix.
 }
 
 
-## Write a short comment describing this function
+## The second function, defined below, will actually compute
+# the value of the matrix's inverse, after first making sure 
+# the inverse hasn't already been calculated and stored in 
+# the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  
+  if(!is.null(inv)) { # If cache for inverse isn't empty...
+    
+    message("getting cached data")
+    return(inv) # ...get the cached value. No work needed!
+  }
+  
+  ## If cache IS empty, then the code below does the job,
+  # using the functions in the special object we created.
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv) # Stores result in cache for future use.
+  
+  ## Finally, we return the value for the inverse, and we
+  # are all set!
+  inv
 }
+
+## Good work!
